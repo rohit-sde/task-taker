bff037f1493dea6fa71c46c859ed02113a9522d9
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-23 11:08:44 +0530

    add exit ani for form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
-import NewChallenge from './NewChallenge.jsx';
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
-  const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
+    const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
 
-  function handleStartAddNewChallenge() {
-    setIsCreatingNewChallenge(true);
-  }
+    function handleStartAddNewChallenge() {
+        setIsCreatingNewChallenge(true);
+    }
 
-  function handleDone() {
-    setIsCreatingNewChallenge(false);
-  }
+    function handleDone() {
+        setIsCreatingNewChallenge(false);
+    }
 
-  return (
-    <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+    return (
+        <>
+            <AnimatePresence>
+                {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+            </AnimatePresence>
 
-      <header id="main-header">
-        <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
-          Add Challenge
-        </button>
-      </header>
-    </>
-  );
+            <header id="main-header">
+                <h1>Your Challenges</h1>
+                <button onClick={handleStartAddNewChallenge} className="button">
+                    Add Challenge
+                </button>
+            </header>
+        </>
+    );
 }
